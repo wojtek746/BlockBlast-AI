e1ab04b58c2ae50e44f3e2d03c978426bffff513
wojtek746 <wojtek7462@gmail.com>
2025-09-10 10:15:28 +0200

    easier sending

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import subprocess
 import time
 import os
-from http.client import responses
 import cv2
 import numpy as np
 import pytesseract
@@ -10,93 +9,81 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 proc = None
 
+fragments = []
+coords = []
+sent = {}
+
 def tap(x, y):
     os.system(f"adb shell input tap {str(x)} {str(y)}")
 
 def swipe(x1, y1, x2, y2, duration_ms=300):
     os.system(f"adb shell input swipe {str(x1)} {str(y1)} {str(x2)} {str(y2)} {str(duration_ms)}")
 
-def sending(filename='screen.png', url='https://logical-raccoon-keen.ngrok-free.app/zdj'):
+def sending(filename='screen.png'):
     screen = cv2.imread(filename)
-    send(screen, 94, 644, url)
-    send(screen, 213, 644, url)
-    send(screen, 333, 644, url)
-    send(screen, 452, 644, url)
-    send(screen, 571, 644, url)
-    send(screen, 690, 644, url)
-    send(screen, 809, 644, url)
-    send(screen, 928, 644, url)
-    send(screen, 94, 763, url)
-    send(screen, 213, 763, url)
-    send(screen, 333, 763, url)
-    send(screen, 452, 763, url)
-    send(screen, 571, 763, url)
-    send(screen, 690, 763, url)
-    send(screen, 809, 763, url)
-    send(screen, 928, 763, url)
-    send(screen, 94, 882, url)
-    send(screen, 213, 882, url)
-    send(screen, 333, 882, url)
-    send(screen, 452, 882, url)
-    send(screen, 571, 882, url)
-    send(screen, 690, 882, url)
-    send(screen, 809, 882, url)
-    send(screen, 928, 882, url)
-    send(screen, 94, 1002, url)
-    send(screen, 213, 1002, url)
-    send(screen, 333, 1002, url)
-    send(screen, 452, 1002, url)
-    send(screen, 571, 1002, url)
-    send(screen, 690, 1002, url)
-    send(screen, 809, 1002, url)
-    send(screen, 928, 1002, url)
-    send(screen, 94, 1121, url)
-    send(screen, 213, 1121, url)
-    send(screen, 333, 1121, url)
-    send(screen, 452, 1121, url)
-    send(screen, 571, 1121, url)
-    send(screen, 690, 1121, url)
-    send(screen, 809, 1121, url)
-    send(screen, 928, 1121, url)
-    send(screen, 94, 1240, url)
-    send(screen, 213, 1240, url)
-    send(screen, 333, 1240, url)
-    send(screen, 452, 1240, url)
-    send(screen, 571, 1240, url)
-    send(screen, 690, 1240, url)
-    send(screen, 809, 1240, url)
-    send(screen, 928, 1240, url)
-    send(screen, 94, 1360, url)
-    send(screen, 213, 1360, url)
-    send(screen, 333, 1360, url)
-    send(screen, 452, 1360, url)
-    send(screen, 571, 1360, url)
-    send(screen, 690, 1360, url)
-    send(screen, 809, 1360, url)
-    send(screen, 928, 1360, url)
-    send(screen, 94, 1479, url)
-    send(screen, 213, 1479, url)
-    send(screen, 333, 1479, url)
-    send(screen, 452, 1479, url)
-    send(screen, 571, 1479, url)
-    send(screen, 690, 1479, url)
-    send(screen, 809, 1479, url)
-    send(screen, 928, 1479, url)
 
-def send(screen, x, y, url):
+    xs = [94,213,333,452,571,690,809,928]
+    ys = [644,763,882,1002,1121,1240,1360,1479]
+
+    global fragments, coords, sent
+    fragments = []
+    coords = []
+    sent = {}
+
+    for yi, y in enumerate(ys):
+        row_frag = []
+        row_coords = []
+        for xi, x in enumerate(xs):
+            frag = screen[y:y + 56, x:x + 57]
+            row_frag.append(frag)
+            row_coords.append((x, y))
+        fragments.append(row_frag)
+        coords.append(row_coords)
+
+    grid = np.zeros((len(ys) * 60, len(xs) * 60, 3), dtype=np.uint8)
+    for i in range(len(ys)):
+        for j in range(len(xs)):
+            grid[i * 60:i * 60 + 56, j * 60:j * 60 + 57] = fragments[i][j]
+
+    cv2.imshow("board", grid)
+    cv2.setMouseCallback("board", on_click)
+
+    while True:
+        if cv2.waitKey(50) == 13:  # Enter
+            break
+
+    cv2.destroyAllWindows()
+
+    print("wysyłanie reszty z 0 (to może trochę potrwać)")
+
+    for i in range(len(ys)):
+        for j in range(len(xs)):
+            if (coords[i][j] not in sent):
+                send(fragments[i][j], '0', *coords[i][j])
+
+    print("wysłano wszystko")
+
+def send(fragment, key, x, y):
     import response
-    fragment = screen[y:y+56, x:x+57]
-    cv2.imshow("fragment", fragment)
-    key = chr(cv2.waitKey(0))
-    cv2.destroyWindow("fragment")
-    if key in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        response = response.response(fragment, key, url)
-        if response.status_code != 200:
-            print(response.text)
-            send(screen, x, y, url)
-    else:
-        print('zły numer')
-        send(screen, x, y, url)
+    response = response.response(fragment, key)
+    if response.status_code != 200:
+        print(response.text)
+        return False
+    sent[(x, y)] = True
+    if key != '0':
+        print(f"wysłano {key}")
+    return True
+
+def on_click(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        idx = (y // 60, x // 60)
+        if idx[0] < len(fragments) and idx[1] < len(fragments[0]):
+            fragment = fragments[idx[0]][idx[1]]
+            x, y = coords[idx[0]][idx[1]]
+            print(f"Klik {idx}, czekam na cyfrę 1-9")
+            key = chr(cv2.waitKey(0))
+            if key in '123456789':
+                send(fragment, key, x, y)
 
 def get_screenshot(filename='screen.png'):
     os.system("adb shell screencap -p /sdcard/screen.png")
