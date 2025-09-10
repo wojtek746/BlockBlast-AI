b52e97f6e8e18210fdacc741e59ebbafa9a76e68
wojtek746 <Wojtek7462@gmail.com>
2025-09-10 15:25:47 +0200

    adding learning AI for numbers

---
+++ learnNumberAI.py
@@ -0,0 +1,60 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class NumberNet(nn.Module):
+    def __init__(self, input_shape=(1,56,57)):
+        super(NumberNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.pool = nn.MaxPool2d(2, 2, ceil_mode=True)
+
+        self.fc1 = nn.Linear(13440, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((56, 57)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+dataset = datasets.ImageFolder(root=r"C:\Users\Wojtek746\Desktop\AI\png", transform=transform)
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+device = torch.device("cuda")
+model = NumberNet().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(5):  # np. 5 epok
+    model.train()
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+    print(f"Epoch {epoch+1}, Loss: {loss.item():.4f}")
+
+torch.save(model.state_dict(), "number_net.pth")
---
+++ test.py
@@ -0,0 +1,5 @@
+import torch
+print("Torch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("GPU:", torch.cuda.get_device_name(0))
